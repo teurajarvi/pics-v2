7c21279691f5692f3db84d82b42b2634db8ef10f
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-05 15:10:50 +0300

    Added ref to the picture

--- pics-v2/pics-v2/src/components/ImageCard.js
+++ pics-v2/pics-v2/src/components/ImageCard.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.pictureRef = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.pictureRef);
+  }
+
   render() {
     const { description, urls } = this.props.picture;
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.pictureRef} alt={description} src={urls.regular} />
       </div>
     );
   }
